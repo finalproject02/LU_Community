bd2761af892121f9f2b9b771676125d47a4afddf
mdjahedahmed666 <lucommunity3@gmail.com>
2021-09-21 00:10:05 +0600

    sidebar with nav complete

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import AdminNavbar from './components/AdminNavbar/AdminNavbar';
+import Home from './pages/Home/Home';
+import AdmissionRequest from './pages/AdmissionRequest/AdmissionRequest';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <AdminNavbar></AdminNavbar>
+        <Switch>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/admissionRequest">
+            <AdmissionRequest></AdmissionRequest>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
