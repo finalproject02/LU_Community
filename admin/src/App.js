7d147452a782545483df64f4a0e3e142d84f1e68
mdjahedahmed666 <lucommunity3@gmail.com>
2021-09-22 14:09:35 +0600

    admission reqwuest almost complete need some more to complete

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,7 +4,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import AdminNavbar from './components/AdminNavbar/AdminNavbar';
 import Home from './pages/Home/Home';
