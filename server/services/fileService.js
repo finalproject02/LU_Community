c974d0da1d1d1288065ec2a5b2742e5e6fe42711
mdjahedahmed666 <lucommunity3@gmail.com>
2021-10-04 13:16:14 +0600

    added file

--- server/services/fileService.js
+++ server/services/fileService.js
@@ -1,12 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import Grid from "gridfs-stream";
-import {GridFsStorage} from "multer-gridfs-storage";
+import { GridFsStorage } from "multer-gridfs-storage";
 import multer from "multer";
 
 const router = express.Router()
 
-const mongoURI = 'mongodb://localhost:27017/lucommunity'
+const mongoURI = 'mongodb://localhost:27017/LU_Community'
 const conn = mongoose.createConnection(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -35,14 +35,14 @@ const storage = new GridFsStorage({
     }
 });
 
-const upload = multer({storage});
-router.post('/storage', upload.single('file') ,(req, res) => {
+const upload = multer({ storage });
+router.post('/storage', upload.single('file'), (req, res) => {
     res.json('upload success')
 });
 
 router.get('/storage/:filename', async (req, res) => {
-    const {filename} = req.params
-    await gfs.files.findOne({filename}, (err, file) => {
+    const { filename } = req.params
+    await gfs.files.findOne({ filename }, (err, file) => {
         if (!file || file.length === 0) {
             return res.status(404).json({
                 err: 'No file exists'
@@ -63,7 +63,7 @@ router.delete('/storage/:filename', async (req, res) => {
                 err: 'No file exists'
             })
         } else {
-            gfs.remove({_id: file._id, root: 'storage' }, (err, gridStore) => {
+            gfs.remove({ _id: file._id, root: 'storage' }, (err, gridStore) => {
                 if (err) return console.log('error')
                 res.send('success')
             })
