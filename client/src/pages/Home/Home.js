ffc77df83f6ead2132e83d8768f354adf8eb83d7
mdjahedahmed666 <mdjahedahmed666@gmail.com>
2021-10-24 13:05:27 +0600

    ....

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,7 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 const Home = () => {
 
   return (
-    <div>
+    <div className="overflow-hidden">
       <Header />
       <MainNavbar />
       <MainCarousel />
