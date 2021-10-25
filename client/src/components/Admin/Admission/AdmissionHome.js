a6a5b4a9653361e466d6cc703d1d3887f2491f2f
cse21program <cse21program@gmail.com>
2021-10-25 14:24:38 -0700

    'payment demo completed'

--- client/src/components/Admin/Admission/AdmissionHome.js
+++ client/src/components/Admin/Admission/AdmissionHome.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {Card, Col, Container, Form, Row, Table} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import AdminNavbar from '../AdminNavbar/AdminNavbar';
+import {useSelector} from "react-redux";
 
 const AdmissionHome = () => {
+    const { people } = useSelector(state => state.people);
+    const admissionFeePaid = people?.filter(person => person.position === 'paid admission fee');
     return (
         <div>
             <AdminNavbar />
@@ -51,136 +54,43 @@ const AdmissionHome = () => {
                                         <Table striped bordered hover>
                                             <thead>
                                             <tr>
-                                                <th scope="col">ID</th>
                                                 <th scope="col">Name</th>
                                                 <th scope="col">Email</th>
                                                 <th scope="col">Phone</th>
                                                 <th scope="col">Department</th>
-                                                <th scope="col">Refrence Number</th>
+                                                <th scope="col">Reference Number</th>
                                                 <th scope="col">Payment</th>
                                                 <th scope="col">Approve</th>
                                             </tr>
                                             </thead>
                                             <tbody>
-                                            <tr>
-                                                <td>1732020020</td>
-                                                <td>Md Jahed Miah</td>
-                                                <td>mdjahedahmed12@gmail.com</td>
-                                                <td>0179713005</td>
-                                                <td>CSE</td>
-                                                <td>12345678</td>
-                                                <td>20000</td>
-                                                <td>
-                                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                        <Form.Check type="checkbox" />
-                                                    </Form.Group>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>1732020020</td>
-                                                <td>Md Jahed Miah</td>
-                                                <td>mdjahedahmed12@gmail.com</td>
-                                                <td>0179713005</td>
-                                                <td>CSE</td>
-                                                <td>12345678</td>
-                                                <td>20000</td>
-                                                <td>
-                                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                        <Form.Check type="checkbox" />
-                                                    </Form.Group>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>1732020020</td>
-                                                <td>Md Jahed Miah</td>
-                                                <td>mdjahedahmed12@gmail.com</td>
-                                                <td>0179713005</td>
-                                                <td>CSE</td>
-                                                <td>12345678</td>
-                                                <td>20000</td>
-                                                <td>
-                                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                        <Form.Check type="checkbox" />
-                                                    </Form.Group>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>1732020020</td>
-                                                <td>Md Jahed Miah</td>
-                                                <td>mdjahedahmed12@gmail.com</td>
-                                                <td>0179713005</td>
-                                                <td>CSE</td>
-                                                <td>12345678</td>
-                                                <td>20000</td>
-                                                <td>
-                                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                        <Form.Check type="checkbox" />
-                                                    </Form.Group>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>1732020020</td>
-                                                <td>Md Jahed Miah</td>
-                                                <td>mdjahedahmed12@gmail.com</td>
-                                                <td>0179713005</td>
-                                                <td>CSE</td>
-                                                <td>12345678</td>
-                                                <td>20000</td>
-                                                <td>
-                                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                        <Form.Check type="checkbox" />
-                                                    </Form.Group>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>1732020020</td>
-                                                <td>Md Jahed Miah</td>
-                                                <td>mdjahedahmed12@gmail.com</td>
-                                                <td>0179713005</td>
-                                                <td>CSE</td>
-                                                <td>12345678</td>
-                                                <td>20000</td>
-                                                <td>
-                                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                        <Form.Check type="checkbox" />
-                                                    </Form.Group>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>1732020020</td>
-                                                <td>Md Jahed Miah</td>
-                                                <td>mdjahedahmed12@gmail.com</td>
-                                                <td>0179713005</td>
-                                                <td>CSE</td>
-                                                <td>12345678</td>
-                                                <td>20000</td>
-                                                <td>
-                                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                        <Form.Check type="checkbox" />
-                                                    </Form.Group>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>1732020020</td>
-                                                <td>Md Jahed Miah</td>
-                                                <td>mdjahedahmed12@gmail.com</td>
-                                                <td>0179713005</td>
-                                                <td>CSE</td>
-                                                <td>12345678</td>
-                                                <td>20000</td>
-                                                <td>
-                                                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                        <Form.Check type="checkbox" />
-                                                    </Form.Group>
-                                                </td>
-                                            </tr>
+                                            {admissionFeePaid?.map(admissionReq => (
+                                                <tr>
+                                                    <td>{admissionReq.name}</td>
+                                                    <td>{admissionReq.email}</td>
+                                                    <td>{admissionReq.mobile}</td>
+                                                    <td>{admissionReq.program_name}</td>
+                                                    <td>{admissionReq.reference_no}</td>
+                                                    <td>{admissionReq.payment_history.map(history => history.admission_fee).toString()}</td>
+                                                    <td>
+                                                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                                            <Form.Check type="checkbox" />
+                                                        </Form.Group>
+                                                    </td>
+                                                </tr>
+                                            ))}
+
                                             </tbody>
                                         </Table>
+                                        <div className="text-end">
+                                            <button className="btn btn-primary">Accept all</button>
+                                        </div>
                                     </Col>
                                 </Row>
                             </Card.Body>
                         </Card>
                     </Col>
+
                 </Row>
             </Container>
         </div>
